6d94d8946cc64c5a8707be2d9400ac13804b6354
Orhun Tokdemir <dogan.tokdemir@hotmail.com>
2026-07-23 17:52:33 +0300

    Make main method public in Sample class for accessibility

--- src/main/java/org/orhuntokdemir/sample/Sample.java
+++ src/main/java/org/orhuntokdemir/sample/Sample.java
@@ -3,7 +3,7 @@ import org.orhuntokdemir.DB.PostgreManager;
 import java.sql.SQLException;
 
 public class Sample {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Sample sample = new Sample();
         try {
             sample.testTable();
