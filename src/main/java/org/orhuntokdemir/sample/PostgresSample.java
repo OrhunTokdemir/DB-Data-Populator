596e72de0d8794f4dba1c0181fa017eb8568289c
Orhun Tokdemir <dogan.tokdemir@hotmail.com>
2026-07-27 17:09:53 +0300

    #Removed obsolete Samples and created a new sample for PostgreSQL database record

---
+++ src/main/java/org/orhuntokdemir/sample/PostgresSample.java
@@ -0,0 +1,28 @@
+package org.orhuntokdemir.sample;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.orhuntokdemir.DB.DbManager;
+import org.orhuntokdemir.DB.PostgreManager;
+import org.orhuntokdemir.generation.InsertionManager;
+import org.orhuntokdemir.generation.PostgreDataInserter;
+
+import java.sql.SQLException;
+
+public class PostgresSample {
+     static void main() {
+        String url = "jdbc:postgresql://localhost:8003/postgres";//Change this to your database URL if needed
+        Dotenv dotenv = Dotenv.load();
+        String user = dotenv.get("POSTGRES_USER") != null ? dotenv.get("POSTGRES_USER") : "postgres";
+        String password = dotenv.get("POSTGRES_PASSWORD") != null ? dotenv.get("POSTGRES_PASSWORD") : "postgres";
+        //how many records to enter
+        int recordCount = 1000;
+        try (DbManager dbManager = new PostgreManager(url, user, password)) {
+            PostgreDataInserter inserter = new PostgreDataInserter(dbManager);
+            InsertionManager manager = new InsertionManager(inserter);
+
+            manager.run(recordCount, false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
