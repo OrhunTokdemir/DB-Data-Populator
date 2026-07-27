95842aad1574b8f72d4166461134c8dbe8213331
Orhun Tokdemir <dogan.tokdemir@hotmail.com>
2026-07-28 01:48:18 +0300

    Add comments to DataInserter interface to clarify method purposes

--- src/main/java/org/orhuntokdemir/generation/DataInserter.java
+++ src/main/java/org/orhuntokdemir/generation/DataInserter.java
@@ -3,14 +3,18 @@ package org.orhuntokdemir.generation;
 import java.sql.SQLException;
 
 public interface DataInserter {
-
+    //set how many records to insert
     void setRecordCount(int count);
 
+    //drop specified table if exists
     void dropComprehensiveTable() throws SQLException;
 
+    //create comprehensive table with the respective columns and data types
     void createComprehensiveTable() throws SQLException;
 
+    //insert randomized data into the comprehensive table
     void insertComprehensiveData() throws SQLException;
 
+    //act as a wrapper to drop, create and insert data into the comprehensive table
     void createAndInsertComprehensiveData(boolean dropFirst) throws SQLException;
 }
