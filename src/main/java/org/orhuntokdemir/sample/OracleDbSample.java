53629262b68d456682d91ed3c9039162c6ddd8c7
Orhun Tokdemir <dogan.tokdemir@hotmail.com>
2026-07-31 21:49:39 +0300

    Switch to `OracleDbManager`, increase record count to 10,000, and enable `dropFirst` during insertion

--- src/main/java/org/orhuntokdemir/sample/OracleDbSample.java
+++ src/main/java/org/orhuntokdemir/sample/OracleDbSample.java
@@ -2,7 +2,7 @@ package org.orhuntokdemir.sample;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.orhuntokdemir.DB.DbManager;
-import org.orhuntokdemir.DB.PostgreManager;
+import org.orhuntokdemir.DB.OracleDbManager;
 import org.orhuntokdemir.generation.InsertionManager;
 import org.orhuntokdemir.generation.OracleDbInserter;
 import java.sql.SQLException;
@@ -15,13 +15,13 @@ public class OracleDbSample
         String user = dotenv.get("ORACLE_USER") != null ? dotenv.get("ORACLE_USER") : "system";
         String password = dotenv.get("ORACLE_PASSWORD") != null ? dotenv.get("ORACLE_PASSWORD") : "oracle";
         //how many records to enter
-        int recordCount = 1000;
+        int recordCount = 10000;
         try (
-                DbManager dbManager = new PostgreManager(url, user, password)) {
+                DbManager dbManager = new OracleDbManager(url, user, password)) {
             OracleDbInserter inserter = new OracleDbInserter(dbManager);
             InsertionManager manager = new InsertionManager(inserter);
 
-            manager.run(recordCount, false);
+            manager.run(recordCount, true);
         } catch (
                 SQLException e) {
             e.printStackTrace();
