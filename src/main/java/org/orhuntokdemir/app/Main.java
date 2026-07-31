73f55cc08ff31c382fabc97efda5bf07561bc005
Orhun Tokdemir <dogan.tokdemir@hotmail.com>
2026-07-31 19:10:46 +0300

    Call dropComprehensiveTable in InsertionManager when dropFirst is true

--- src/main/java/org/orhuntokdemir/app/Main.java
+++ src/main/java/org/orhuntokdemir/app/Main.java
@@ -10,18 +10,18 @@ import java.sql.SQLException;
 
 
 public class Main {
-    public static void main(String[] args) {
+     static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:8003/postgres";//Change this to your database URL if needed
         Dotenv dotenv = Dotenv.load();
         String user = dotenv.get("POSTGRES_USER") != null ? dotenv.get("POSTGRES_USER") : "postgres";
         String password = dotenv.get("POSTGRES_PASSWORD") != null ? dotenv.get("POSTGRES_PASSWORD") : "postgres";
         //how many records to enter
-        int recordCount = 100;
+        int recordCount = 10000;
         try (DbManager dbManager = new PostgreManager(url, user, password)) {
             PostgreDataInserter inserter = new PostgreDataInserter(dbManager);
             InsertionManager manager = new InsertionManager(inserter);
 
-            manager.run(recordCount, false);
+            manager.run(recordCount, true);
         } catch (SQLException e) {
             e.printStackTrace();
         }
